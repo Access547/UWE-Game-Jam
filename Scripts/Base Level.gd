5293d4fe7958c4c0c693472c9f4a47e26be51966
Access547 <86067971+Access547@users.noreply.github.com>
2024-01-27 21:10:27 +0000

    POOP SCREEN
    GAME OVER

--- Scripts/Base Level.gd
+++ Scripts/Base Level.gd
@@ -2,24 +2,8 @@ extends Node2D
 class_name Level
 
 
-@onready var ttp = $TTP
-@onready var transition = $Transition
-
-
-
-
-func _on_ttp_timeout():
-	print("You shit yourself!")
-
-
-
-func StartTTP(time):
-	ttp.start(time)
-
-
-
-func Transition():
-	transition.play("Transition")
+func _ready():
+	NumberManager.canMove = true
 
 
 

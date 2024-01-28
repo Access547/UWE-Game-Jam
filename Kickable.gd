03756569210386dadb5433b9d050f7abd5e8efd6
GitExcel <sean.harding@live.co.uk>
2024-01-28 00:26:02 +0000

    stuff

--- Kickable.gd
+++ Kickable.gd
@@ -5,10 +5,16 @@ extends Area2D
 @onready var sBody = $StaticBody2D
 @onready var colShape = $CollisionShape2D
 @onready var collision_shape_2d = $StaticBody2D/CollisionShape2D
+@export var sceene:PackedScene
 
 
 func _on_area_entered(area):
 	if area.get_collision_layer() == 8:
+		var instance = sceene.instantiate()
+		get_tree().root.add_child(instance)
+		instance.position = position
+		instance.emitting = true
+		
 		get_tree().get_first_node_in_group("Camera").ApplyShake()
 		audio.pitch_scale = randf_range(0.9, 1.2)
 		audio.play()
